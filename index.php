4d3e90d984fb9a33434c97ed6f12f1c7bb8f4f96
akki <aakash.choudhary.ongraph@gmail.com>
2022-07-06 16:26:56 +0530

    code updated

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $output1 =  exec('pwd');
 echo $output1;
 
 echo "<br/>";
-echo "Current Date of the System is : <br>";
+echo "Current Date of the System is :: <br>";
 echo exec('date');
 
 echo "<br/>";
